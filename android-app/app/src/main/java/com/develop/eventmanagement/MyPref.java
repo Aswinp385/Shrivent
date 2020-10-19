833b291bd124efc5309f4a0fab19f6cd1bb2be68
Aswinp385 <62582651+Aswinp385@users.noreply.github.com>
2020-10-19 12:20:42 +1100

    push notification integration

---
+++ android-app/app/src/main/java/com/develop/eventmanagement/MyPref.java
@@ -0,0 +1,9 @@
+package com.develop.eventmanagement;
+
+public class MyPref {
+
+    public static String prefName = "CPref";
+    public static String isLogin = "isLogin";
+    public static String useremail = "useremail";
+
+}
